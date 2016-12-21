ae51d5939e3c7416035bce7de291302e5bc38332
klee <k.apus@hotmail.com>
2016-12-21 14:10:52 +0100

    Add klee oracle script #6

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,3 +1,5 @@
+#Requires mktemp, dirname, realpath, timeout, basename
+#TODO: assert those
 GH_PROJECT_ROOT=/home/klee/project/testSymExec
 
 CSMITH_RUNTIME=/home/klee/dependencies/csmith/runtime
@@ -5,3 +7,4 @@ INST_LIB_PATH=$GH_PROJECT_ROOT/instrument_lib
 INSTRUMENT_TOOL=$GH_PROJECT_ROOT/src2src/build/tool
 CLANG=clang-3.4
 LINK=llvm-link-3.4
+KLEE=klee
