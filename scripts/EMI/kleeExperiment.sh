dfe8978371dec0dbc8f20a0d776cc2a26d4c8525
Timotej Kapus <k.apus@hotmail.com>
2017-01-11 13:23:13 +0000

    Replaced xargs with parallel #6

--- scripts/EMI/kleeExperiment.sh
+++ scripts/EMI/kleeExperiment.sh
@@ -3,5 +3,5 @@ DIR_NAME=$(dirname "$(realpath $0)")
 source $DIR_NAME/../settings.sh
 INSTRUMENTER=$GH_PROJECT_ROOT/scripts/EMI/deadConditionInject.sh
 COMPILE_AND_RUN=$GH_PROJECT_ROOT/scripts/EMI/kleeCompileAndRun.sh
-ls | grep -P "test\d+\.c" | sort -V | xargs -L1  $DIR_NAME/singleEMIExperiment.sh
+ls | grep -P "test\d+\.c" | sort -V | parallel -L1 --no-notice  $DIR_NAME/singleEMIExperiment.sh
 
