735b1fdae0833cebba1260ceb9ed6688911beec0
Timotej Kapus <k.apus@hotmail.com>
2017-01-11 13:10:05 +0000

    Klee symbolised experiments #6

--- scripts/EMI/kleeCompileAndRun.sh
+++ scripts/EMI/kleeCompileAndRun.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+DIR_NAME=$(dirname "$(realpath $0)")
+source $DIR_NAME/../settings.sh
+
 BC_FILE=$(mktemp)
-clang-3.4 -xc -I$CSMITH_RUNTIME -o $BC_FILE -c -emit-llvm $1 > /dev/null &&\
-timeout 100 klee $BC_FILE
+LINKED_BC_FILE=$(mktemp)
+$CLANG -xc -I$INST_LIB_PATH -I$CSMITH_RUNTIME -o $BC_FILE -c -emit-llvm $1 > /dev/null &&\
+$LINK -o=$LINKED_BC_FILE $BC_FILE $INST_LIB_PATH/klee/build/*.bc
+timeout 100 klee $LINKED_BC_FILE
 EXIT_STATUS=$?
-rm $BC_FILE
+
+#Prints the function call trace to stdout and deletes it for cleanup. 
+cat $2 &&\
+rm $2
+
+rm $BC_FILE $LINKED_BC_FILE
 if [ $EXIT_STATUS == "124" ];
 then
     echo "timeout"
--- scripts/EMI/nativeCompileAndRun.sh
+++ scripts/EMI/nativeCompileAndRun.sh
@@ -5,8 +5,8 @@ source $DIR_NAME/../settings.sh
 
 O_FILE=$(mktemp)
 LINKED_FILE=$(mktemp)
-clang-3.4 -xc -c -I$INST_LIB_PATH -I$CSMITH_RUNTIME -o $O_FILE $1 > /dev/null &&\
-clang-3.4 -o $LINKED_FILE $O_FILE $INST_LIB_PATH/native/build/*.o > /dev/null &&\
+$CLANG -xc -c -I$INST_LIB_PATH -I$CSMITH_RUNTIME -o $O_FILE $1 > /dev/null &&\
+$CLANG -o $LINKED_FILE $O_FILE $INST_LIB_PATH/native/build/*.o > /dev/null &&\
 timeout 1 $LINKED_FILE
 EXIT_STATUS=$?
 #Prints the function call trace to stdout and deletes it for cleanup. 
