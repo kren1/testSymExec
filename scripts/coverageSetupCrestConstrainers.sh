2ef1d2d1dff431454bdd5b0a41a986f86eaf0662
Timotej Kapus <k.apus@hotmail.com>
2017-01-21 14:18:36 +0000

    Added some coverage oracle capability

---
+++ scripts/coverageSetupCrestConstrainers.sh
@@ -0,0 +1,7 @@
+# Sets up a constrainer experiment with crest
+INSTRUMENTER=cp
+COMPILE_AND_RUN_1=~/projects/testSymExec/scripts/CaR/coverage/nativeCompileAndRun.sh
+COMPILE_AND_RUN_2=~/projects/testSymExec/scripts/CaR/coverage/crestCompileAndRun.sh
+export INSTRUMENTER
+export COMPILE_AND_RUN_1
+export COMPILE_AND_RUN_2
