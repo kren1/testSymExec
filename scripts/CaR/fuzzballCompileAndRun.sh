f46c9500bf3b97f63bd8a2604bab10e9db8a1c3f
tim <tim@tim-VirtualBox.(none)>
2017-01-28 19:36:42 +0000

    Add instr. lib aswell

--- scripts/CaR/fuzzballCompileAndRun.sh
+++ scripts/CaR/fuzzballCompileAndRun.sh
@@ -13,7 +13,7 @@ $CLANG -o $LINKED_FILE -m32 $O_FILE $INST_LIB_PATH/fuzzball/build/*.o 2> $NULL &
 ADDRESSES=$(objdump -d $LINKED_FILE | grep -P "call [\s\w\d]+ <symbolic\d+" \
  | awk '{print  "-skip-call-ret-symbol 0x" substr($1, 1, length($1)-1) "=n"}' | tail -n1 |  head -n1) &&\
 ADDRESSES=$(objdump -D $LINKED_FILE | grep -P "magic_symbols" \
- | awk '{print  "-symbolic-string 0x"i $1 "+16"}' | tail -n1 |  head -n1) &&\
+ | awk '{print  "-symbolic-region 0x"i $1 "+16"}' | tail -n1 |  head -n1) &&\
 #echo $ADDRESSES &&\
 START=$(date +%s.%N) &&\
 timeout 100 $FUZZBALL  -solver-path /home/tim/dependencies/fuzzball/stp/stp/build/stp -linux-syscalls $ADDRESSES $LINKED_FILE -- $LINKED_FILE
