3e6e87b8ad9ba87a17a04940309ae40187c39e1f
tim <k.apus@hotmail.com>
2017-01-28 20:33:56 +0000

    Imporve run general experiment

--- scripts/runGeneralExperiment.sh
+++ scripts/runGeneralExperiment.sh
@@ -4,6 +4,6 @@ DIR_NAME=$(dirname "$(realpath $0)")
 EXPERIMENT_LOG=${PWD##*/}.log
 export EXPERIMENT_LOG
 
-ls | grep -P "test\d+\.c" | sort -V | parallel -L1 --no-notice  $DIR_NAME/generalSingleExperiment.sh 2> $NULL
+ls | grep -P "test\d+\.c$" | sort -V | parallel -L1 --no-notice  $DIR_NAME/generalSingleExperiment.sh 2> $NULL
 
 $DIR_NAME/utils/getEnvConfig.sh $EXPERIMENT_LOG
