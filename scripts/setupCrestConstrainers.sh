572d75e21d5575e7fedd109a3cb368f52e717f21
Timotej Kapus <k.apus@hotmail.com>
2017-02-11 02:28:59 +0000

    Make setupscripts more install independant

--- scripts/setupCrestConstrainers.sh
+++ scripts/setupCrestConstrainers.sh
@@ -1,7 +1,12 @@
 # Sets up a constrainer experiment with crest
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+source $DIR/settings.sh
+
+
 INSTRUMENTER=cp
-COMPILE_AND_RUN_1=~/projects/testSymExec/scripts/CaR/nativeCompileAndRun.sh
-COMPILE_AND_RUN_2=~/projects/testSymExec/scripts/CaR/crestCompileAndRun.sh
+COMPILE_AND_RUN_1=${GH_PROJECT_ROOT}/scripts/CaR/nativeCompileAndRun.sh
+COMPILE_AND_RUN_2=${GH_PROJECT_ROOT}/scripts/CaR/crestCompileAndRun.sh
 export INSTRUMENTER
 export COMPILE_AND_RUN_1
 export COMPILE_AND_RUN_2
