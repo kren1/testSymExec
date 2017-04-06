dd79d957a926e24574d44d41be1ef7ccae6f3294
Timotej Kapus <k.apus@hotmail.com>
2017-04-06 12:07:13 +0100

    Fix the nested if test #7

--- src2src/tests/swapBranches/nestedifels.c
+++ src2src/tests/swapBranches/nestedifels.c
@@ -3,7 +3,7 @@
 // RUN: %clang -xc -o %t2 %t5
 // RUN: %t1 > %t3
 // RUN: %t2 > %t4
-// RUN: diff %t3 %t4 && bash -c ' diff %s %t5 | grep "i < 0"'
+// RUN: diff %t3 %t4 && bash -c ' diff %s %t5 | grep "i > 2"'
 #include <stdio.h>
 #include <stdbool.h>
 int main() {
