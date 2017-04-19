779862409f2cf222023411fb6864c468b34b08c3
Timotej Kapus <k.apus@hotmail.com>
2017-04-19 15:21:29 +0100

    Add parallel capability to setup experiment

--- scripts/SetupExperiment.sh
+++ scripts/SetupExperiment.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #USAGE ./SetupExperiment dirname
+if [ -z "$1" ]; then
+echo "Please give folder name as first argument. Exiting ..."
+exit 1
+fi
 
 DIR_NAME=$(dirname "$(realpath $0)")
 source $DIR_NAME/settings.sh
@@ -7,7 +11,15 @@ mkdir $1
 cd $1
 #Making the system status dump
 $DIR_NAME/utils/getEnvConfig.sh experimentConfigdump.txt
+NODE_LIST_FILE=$DIR_NAME/nodelistt
+if [ -f $NODE_LIST_FILE ]; then
+echo "using parallel"
+PARALLEL="--sshloginfile $NODE_LIST_FILE --env '_'"
+else
+PARALLEL=""
+fi
+
 
 #seq 10 | xargs -L1 -I{} csmith -o test{}.c
-seq 100 | parallel -L1 -I{}  $DIR_NAME/utils/generateTestCase.sh test{}.c 2> /dev/null
+seq 10 | parallel $PARALLEL  --bibtex -L1 -I{}  "$DIR_NAME/utils/generateTestCase.sh test{}.c 2> /dev/null"
 
