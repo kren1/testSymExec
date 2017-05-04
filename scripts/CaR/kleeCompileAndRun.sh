5564358c55a11999e930f4275d88a8a53654ff16
Timotej Kapus <k.apus@hotmail.com>
2017-05-04 13:19:04 +0100

    Made klee timeouting stronger

--- scripts/CaR/kleeCompileAndRun.sh
+++ scripts/CaR/kleeCompileAndRun.sh
@@ -2,16 +2,16 @@
 #USAGE ./kleeCompileAndRun test1.c test1.c
 DIR_NAME=$(dirname "$(realpath $0)")
 source $DIR_NAME/../settings.sh
-set -x
 
 BC_FILE=$(mktemp)
 LINKED_BC_FILE=$(mktemp)
+#need to removei before klee is called so klee can recreate 
 KLEE_OUT_DIR=$(mktemp -d)
 $CLANG -xc -I$INST_LIB_PATH -I$CSMITH_RUNTIME -o $BC_FILE -c -emit-llvm $1 2> /dev/null &&\
 $LINK -o=$LINKED_BC_FILE $BC_FILE $INST_LIB_PATH/$LIB_CHOICE/*.bc &&\
 START=$(date +%s.%N) &&\
-rmdir $KLEE_OUT_DIR &&\ #need to remove so klee can recreate 
-timeout 100 klee -output-dir=$KLEE_OUT_DIR $LINKED_BC_FILE
+rmdir $KLEE_OUT_DIR &&\
+timeout -s SIGKILL 120 klee -max-time=100 -allow-external-sym-calls  -output-dir=$KLEE_OUT_DIR $LINKED_BC_FILE
 EXIT_STATUS=$?
 DURATION=$(echo "$(date +%s.%N) - $START" | bc) &&\
 
