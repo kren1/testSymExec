8f4e445d51730a2e244d409282d899100247bdc4
Timotej Kapus <k.apus@hotmail.com>
2017-05-09 01:55:03 +0100

    Clean swpa branch transformer

--- src2src/src/SwapBranchesTransformer.cpp
+++ src2src/src/SwapBranchesTransformer.cpp
@@ -23,21 +23,9 @@ public:
         return;
       }
 
-      std::string then;
-      std::string els;
-      std::string cond;
-      std::string all;
+     std::string all;
       
-      llvm::raw_string_ostream::raw_string_ostream ss(then);
-      llvm::raw_string_ostream::raw_string_ostream sse(els);
-      llvm::raw_string_ostream::raw_string_ostream ssc(cond);
       llvm::raw_string_ostream::raw_string_ostream ssa(all);
-
-      IfS->getThen()->printPretty(ss,NULL,Policy,0 );
-      IfS->getElse()->printPretty(sse,NULL,Policy,0 );
-      ssc << "!( ";
-      IfS->getCond()->printPretty(ssc,NULL,Policy,0);
-      ssc << " -)";
 
       ssa << "if( !(";
       IfS->getCond()->printPretty(ssa,NULL,Policy,0);
@@ -49,12 +37,6 @@ public:
       
       Rewrite.ReplaceText(IfS->getSourceRange(), StringRef(ssa.str()));
 
-      //Rewrite.ReplaceText(IfS->getThen()->getSourceRange(), StringRef(sse.str()));
-      //Rewrite.ReplaceText(IfS->getElse()->getSourceRange(), StringRef(ss.str()) );
-      //Rewrite.ReplaceText(IfS->getCond()->getSourceRange(), StringRef(ssc.str()) );
-
-      //Rewrite.InsertTextBefore(IfS->getCond()->getLocStart(), "!(");
-      //Rewrite.InsertTextAfter(IfS->getCond()->getLocEnd().getLocWithOffset(1), ")");
       visitedIfs.insert(IfS);
       
     }
