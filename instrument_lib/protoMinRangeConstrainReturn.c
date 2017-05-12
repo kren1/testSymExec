5a6a5a8e042425689d84e240b38d8db8b467f43d
Timotej Kapus <k.apus@hotmail.com>
2017-05-12 03:49:52 +0100

    Finished proto min range

--- instrument_lib/protoMinRangeConstrainReturn.c
+++ instrument_lib/protoMinRangeConstrainReturn.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <fcntl.h>
 char __klee__instr_filename;
 unsigned b;
 int safe_add_func_int32_t_s_s_si2;
@@ -31,7 +35,12 @@ int main() {
   long prev = ub + 10;
 
   printf("#0 lb: %ld, ub: %ld\n", lb, ub);
-  while(prev - ub > 1) {
+  
+  char test_case_id[20];
+  snprintf(&test_case_id, 20, "%d-", rand());
+  printf("test: %s", test_case_id);
+
+  while(prev - ub > 0) {
 //  printf("\t#a lb: %ld, ub: %ld, prev: %ld\n", lb, ub, prev);
     if(h < ub){
        prev = ub;
@@ -47,22 +56,57 @@ int main() {
 
   }
 
-  printf("#1 lb: %ld, ub: %ld\n", lb, ub);
+//  printf("#1 lb: %ld, ub: %ld\n", lb, ub);
   //searching the lowest lb
   long ubForlb = ub;
   lb = min_int;
   prev = lb - 10;
-  while(lb - prev > 2) {
+  while(lb - prev > 0) {
     if(h > lb) {
         prev = lb;
         lb = lb + (ubForlb - lb) / 2;
     } else {
        ubForlb = lb;
        lb = prev; 
+       prev = lb - 2;
     }
   }
-  printf("#2 lb: %ld, ub: %ld\n", lb, ub);
+//  printf("#2 lb: %ld, ub: %ld\n", lb, ub);
+  FILE *fp = fopen("locks.file", "rb");
 
+//  printf("fp: %p\n", fp);
+  if(fp != NULL){
+  
+  /*char line[20];
+  while(getline(&line, 20, fp) != -1) {
+    printf("read line %s", line);
+    if(strncmp(line,test_case_id,20) == 0) {
+        fclose(fp);
+        klee_silent_exit(0);
+    }
+  }*/
+ // char fil[201];
+//  for(char*p = fil; p < fil+200;p++) p = 0;
+  char *fil= calloc(201,1);
+  ssize_t rd = fread(fil, 1, 200, fp);
+  int berr = errno;
+//  printf("fread %d eerrno %d\n", rd, berr);
+  if(rd > 0) {
+ // printf("data %s \n", fil);
+  if(strstr(fil,test_case_id) != NULL) {
+    fclose(fp);
+    klee_silent_exit(0);
+  }
+}
+  fclose(fp);
+}
+
+  printf("#3 lb: %ld, ub: %ld\n", lb, ub);
+  printf("GOOD BRANCH\n");
+  fp = fopen("locks.file","a");
+
+  fprintf(fp, "%s", test_case_id);
+  fclose(fp);
   printf("g_346: h %d\n", h);
   printf("g_886: g %d\n", g);
 
