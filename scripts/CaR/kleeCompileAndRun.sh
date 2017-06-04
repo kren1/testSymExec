fcfc9974b44e66db949656df926d0493dd2f89ef
Timotej Kapus <k.apus@hotmail.com>
2017-06-04 18:09:59 +0100

    Add the abilitz to chose where klee output-s the files

--- scripts/CaR/kleeCompileAndRun.sh
+++ scripts/CaR/kleeCompileAndRun.sh
@@ -3,14 +3,16 @@
 DIR_NAME=$(dirname "$(realpath $0)")
 source $DIR_NAME/../settings.sh
 
+
 BC_FILE=$(mktemp)
 LINKED_BC_FILE=$(mktemp)
 #need to removei before klee is called so klee can recreate 
-KLEE_OUT_DIR=$(mktemp -d)
-$CLANG -xc -I$INST_LIB_PATH -I$CSMITH_RUNTIME -o $BC_FILE -c -emit-llvm $1 2> /dev/null &&\
+KLEE_OUT_DIR_TEMP=$(mktemp -d)
+KLEE_OUT_DIR=${KLEE_DIR:-$KLEE_OUT_DIR_TEMP}
+$CLANG -xc -I$INST_LIB_PATH -I$CSMITH_RUNTIME $OTHER_FLAGS -o $BC_FILE -c -emit-llvm $1 2> /dev/null &&\
 $LINK -o=$LINKED_BC_FILE $BC_FILE $INST_LIB_PATH/$LIB_CHOICE/*.bc &&\
+rm -r $KLEE_OUT_DIR 
 START=$(date +%s.%N) &&\
-rmdir $KLEE_OUT_DIR &&\
 timeout -s SIGKILL 520 timeout 500 klee-new -emit-all-errors -use-forked-solver=0 -max-time=590 -watchdog -search=dfs -libc=none -allow-external-sym-calls  -output-dir=$KLEE_OUT_DIR $LINKED_BC_FILE
 EXIT_STATUS=$?
 DURATION=$(echo "$(date +%s.%N) - $START" | bc) &&\
@@ -23,7 +25,7 @@ rm $(basename $2).out.lock /tmp/$(basename $2).out.lock 2> /dev/null
 klee-stats $KLEE_OUT_DIR >&2
 
 rm $BC_FILE $LINKED_BC_FILE
-rm -r $KLEE_OUT_DIR
+rm -r $KLEE_OUT_DIR_TEMP
 if [ $EXIT_STATUS == "124" -o $EXIT_STATUS == "137" ];
 then
     echo "timeout" >> $2.info
