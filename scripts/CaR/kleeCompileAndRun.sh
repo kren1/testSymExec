fd8d1b3763fc35bdc5ab7734789a71f0ba85cd9c
Timotej Kapus <k.apus@hotmail.com>
2017-06-05 17:10:45 +0100

    improve klee car

--- scripts/CaR/kleeCompileAndRun.sh
+++ scripts/CaR/kleeCompileAndRun.sh
@@ -2,6 +2,8 @@
 #USAGE ./kleeCompileAndRun test1.c test1.c
 DIR_NAME=$(dirname "$(realpath $0)")
 source $DIR_NAME/../settings.sh
+NULL=/dev/null
+#NULL=/dev/tty
 
 
 BC_FILE=$(mktemp)
@@ -9,9 +11,10 @@ LINKED_BC_FILE=$(mktemp)
 #need to removei before klee is called so klee can recreate 
 KLEE_OUT_DIR_TEMP=$(mktemp -d)
 KLEE_OUT_DIR=${KLEE_DIR:-$KLEE_OUT_DIR_TEMP}
-$CLANG -xc -I$INST_LIB_PATH -I$CSMITH_RUNTIME $OTHER_FLAGS -o $BC_FILE -c -emit-llvm $1 2> /dev/null &&\
+touch $KLEE_OUT_DIR
+$CLANG -xc -I$INST_LIB_PATH -I$CSMITH_RUNTIME $OTHER_FLAGS -o $BC_FILE -c -emit-llvm $1 2> $NULL &&\
 $LINK -o=$LINKED_BC_FILE $BC_FILE $INST_LIB_PATH/$LIB_CHOICE/*.bc &&\
-rm -r $KLEE_OUT_DIR 
+rm -r $KLEE_OUT_DIR &&\
 START=$(date +%s.%N) &&\
 timeout -s SIGKILL 520 timeout 500 klee-new -emit-all-errors -use-forked-solver=0 -max-time=590 -watchdog -search=dfs -libc=none -allow-external-sym-calls  -output-dir=$KLEE_OUT_DIR $LINKED_BC_FILE
 EXIT_STATUS=$?
