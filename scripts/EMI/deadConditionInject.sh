59e9011e0442ec59f67447b7e4950c38f744f2db
Timotej Kapus <k.apus@hotmail.com>
2017-06-08 12:59:49 +0100

    Handling of other flags in transformers

--- scripts/EMI/deadConditionInject.sh
+++ scripts/EMI/deadConditionInject.sh
@@ -2,4 +2,4 @@
 DIR_NAME=$(dirname "$(realpath $0)")
 source $DIR_NAME/../settings.sh
 INSTRUMENT_TOOL=$GH_PROJECT_ROOT/src2src/build/tool
-$INSTRUMENT_TOOL -extra-arg="-I$INST_LIB_PATH" -extra-arg="-I$CSMITH_RUNTIME" -deadCond $1 --  > $2 
+$INSTRUMENT_TOOL ${OTHER_FLAGS:+"-extra-arg=\"$OTHER_FLAGS\""} -extra-arg="-I$INST_LIB_PATH" -extra-arg="-I$CSMITH_RUNTIME" -deadCond $1 --  > $2 
