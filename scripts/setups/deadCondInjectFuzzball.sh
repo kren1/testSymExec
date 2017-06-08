ed452e4d8ef43c4528a99c3047008b9f1fa8d0a0
Timotej Kapus <k.apus@hotmail.com>
2017-06-08 12:59:21 +0100

    More setups

---
+++ scripts/setups/deadCondInjectFuzzball.sh
@@ -0,0 +1,13 @@
+# Sets up a constrainer experiment with crest
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+source $DIR/../settings.sh
+
+LIB_CHOICE=fuzzball/build/symbolic
+INSTRUMENTER=${GH_PROJECT_ROOT}/scripts/EMI/deadConditionInject.sh
+COMPILE_AND_RUN_1=${GH_PROJECT_ROOT}/scripts/CaR/fuzzballCompileAndRun.sh
+COMPILE_AND_RUN_2=${GH_PROJECT_ROOT}/scripts/CaR/fuzzballCompileAndRun.sh
+export INSTRUMENTER
+export COMPILE_AND_RUN_1
+export COMPILE_AND_RUN_2
+export LIB_CHOICE
